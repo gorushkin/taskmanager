af2cdfbfe2e83175bab349dfe997d6837e71ed21
gorushkin <gorushkin@gmail.com>
2020-11-04 21:53:56 +0300

    Добавил переадресацию после логина

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from 'react';
 import AuthForm from '../components/AuthForm';
+import { ContextUser } from '../user';
+import { useHistory } from 'react-router-dom';
 
 const AuthPage = () => {
+  const user = useContext(ContextUser);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (user.state.email) {
+      console.log('redrect!!!');
+      history.push('/tasks')
+    }
+  }, [user.state]);
+
   return <AuthForm></AuthForm>;
 };
 
