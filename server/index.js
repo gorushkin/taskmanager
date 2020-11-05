c0eacf86f4c5dd24604e1097dd26916684300d90
gorushkin <gorushkin@gmail.com>
2020-11-05 23:15:43 +0300

    temp

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import app from './app.js';
 const port = process.env.PORT || 4000;
 
+console.log(process.env.DB_K);
+
 app.listen(port, () => {
   console.log(`server started at ${port} port`);
 });
