996fb4a97bcf2821ad25cad74b5be97d8e343b1f
gorushkin <gorushkin@gmail.com>
2020-11-09 11:19:06 +0300

    renameTaskForm Добавил кнопку для отмены переименования

--- src/components/taskList/TaskRenameFrom.jsx
+++ src/components/taskList/TaskRenameFrom.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { asyncActions } from '../../slices';
 
-const Form = ({ props: { _id, text, isDone, changeEditStatus, modifyTask } }) => {
+const Form = ({ props: { _id, text, isDone, changeEditStatus } }) => {
   const [value, setValue] = useState(text);
   const inputEl = useRef(null);
   const dispatch = useDispatch();
@@ -23,6 +23,8 @@ const Form = ({ props: { _id, text, isDone, changeEditStatus, modifyTask } }) =>
     changeEditStatus();
   };
 
+  const closeHandler = () => changeEditStatus();
+
   return (
     <li key={_id} className='list-group-item list__item mb-1'>
       <form className='w-100' onSubmit={submithandler}>
@@ -39,8 +41,11 @@ const Form = ({ props: { _id, text, isDone, changeEditStatus, modifyTask } }) =>
             />
           </div>
           <div className='col-auto'>
-            <button type='submit' className='btn btn-lg btn-primary px-sm-5'>
-              Submit
+            <button type='submit' className='btn mr-2 btn-lg btn-primary px-sm-5'>
+              Rename
+            </button>
+            <button onClick={closeHandler} type='button' className='btn btn-lg btn-danger'>
+              &times;
             </button>
           </div>
         </div>
