09240ff5579333e6d383b6f3914cde2007595ea0
gorushkin <gorushkin@gmail.com>
2020-11-11 22:11:33 +0300

    Кнопка проектов Добавил кнопку со сменой на инпут для ввода проекта

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useStore } from 'react-redux';
 import cn from 'classnames';
 
-const ProjectBtn = ({name, _id}, currentProjectId) => {
+const AddProject = ({ handler, btnState }) => {
+  const [value, setValue] = useState('');
+
+  const changeHandler = (e) => {
+    setValue(e.target.value);
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    handler();
+  }
+
+  if (btnState) {
+    return (
+      <form onSubmit={submitHandler}>
+        <div className='input-group'>
+          <input
+            onChange={changeHandler}
+            type='text'
+            className='form-control'
+            placeholder='project name'
+            aria-describedby='basic-addon1'
+            value={value}
+          />
+        </div>
+      </form>
+    );
+  }
+  return (
+    <li className='nav-item'>
+      <button
+        onClick={handler}
+        type='button'
+        className='text-center nav-link btn-block mb-2 text-left btn btn-light'
+      >
+        add project
+      </button>
+    </li>
+  );
+};
+
+const ProjectBtn = ({ name, _id }, currentProjectId) => {
   const projectBtnClass = cn('nav-link btn-block mb-2 text-left btn', {
     'btn-primary': currentProjectId === _id,
     'btn-light': currentProjectId !== _id,
@@ -20,19 +61,16 @@ const ProjectBtn = ({name, _id}, currentProjectId) => {
 const Projects = () => {
   const { projects, currentProjectId } = useSelector((state) => state.projects);
 
-  const addProjectHandler = () => {
-    console.log('add');
+  const [isForm, setIsForm] = useState(false);
 
-  }
+  const addProjectHandler = () => {
+    setIsForm(!isForm);
+  };
 
   return (
     <ul className='nav flex-column'>
       {projects.map((item) => ProjectBtn(item, currentProjectId))}
-      <li className='nav-item'>
-        <button onClick={addProjectHandler} type='button' className='text-center nav-link btn-block mb-2 text-left btn btn-light'>
-          add project
-        </button>
-      </li>
+      <AddProject handler={addProjectHandler} btnState={isForm} />
     </ul>
   );
 };
